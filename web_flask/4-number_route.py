6f58f65efb4cd5c7e290ff810cba701fa2175846
devmutinda <davidkymutinda@gmail.com>
2022-10-15 13:43:31 +0000

    Created route that only takes int argument

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Handles variables passed on URL"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>')
+def python_text(text):
+    """Variable with default value"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """display 'n is a number' only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
