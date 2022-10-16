b8330b95940fcf65d64a9f2dbd1286bb2a829d6a
devmutinda <davidkymutinda@gmail.com>
2022-10-16 14:13:21 +0300

    Updated code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def tear_down():
+def tear_down(exc):
     """Removes the current sqlalchemy session"""
     storage.close()
 
@@ -13,7 +13,7 @@ def tear_down():
 @app.route('/states_list', strict_slashes=False)
 def html_page():
     """Renders a html template"""
-    return render_template('7-states_list.html', states=storage.all())
+    return render_template('7-states_list.html', states=storage.all('State'))
 
 
 if __name__ == "__main__":
