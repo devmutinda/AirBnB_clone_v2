2812b5061e47d85f03b953d69ccf18ea983cc78d
devmutinda <davidkymutinda@gmail.com>
2022-10-16 14:00:28 +0300

    Flask application

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """Removes the current sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def html_page():
+    """Renders a html template"""
+    return render_template('7-states_list.html', states=storage.all())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
