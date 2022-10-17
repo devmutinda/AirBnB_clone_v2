9452193bcca49433fbcacd20e1a7a98923bd84f7
devmutinda <davidkymutinda@gmail.com>
2022-10-17 05:05:44 +0000

    Added documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application
+This module creates a Flask application
+which manipulates storage
 """
 from flask import Flask, render_template
-from models import storage
-from models.state import State
+# from models import storage
+# from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def tear_down(exc):
-    """Removes the current sqlalchemy session"""
+    """
+    Removes the current sqlalchemy session
+    """
     storage.close()
 
 
 @app.route('/states_list/', strict_slashes=False)
 def html_page():
-    """Renders a html template"""
+    """
+    Renders a html template
+    """
+    from models import storage
+    from models.state import State
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
